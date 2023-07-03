93e3a55d9112ce5e3bc01ed6da0b1358d1c77167
tcocco20 <theococco@yahoo.com>
2023-07-02 18:35:27 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
+  );
+}
